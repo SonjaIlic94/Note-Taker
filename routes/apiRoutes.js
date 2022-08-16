8c6535cec0500eceea1c8814c7aa693cdcc47257
sonjailic <sonjailic94@hotmail.com>
2022-08-16 18:40:00 -0400

    finish post note ability

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,36 +2,31 @@ const fs = require('fs');
 const router = require('express').Router();
 const path = require('path');
 const notes = require('../db/notes.json');
+const validate = require('../assets/js/validateNote.js');
 
 // read db.json files + return all saved notes
 router.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/notes.json'))
 });
 
-//post needs to receive a new note to save
-//unique ID, how can npm do this?
-
 //post to notes page
-// router.post('/api/notes', (req, res) => {
-//     console.log(req.body);
-//     req.body.id = notes.length.toString();
-//     res.json(req.body)
-//     // return finished code to post route for respose
-//     return body;
-// });
-// app.post('/api/notes', (req, res) => {
-//     // set id based on what the next index of the array will be
-//     req.body.id = animals.length.toString();
+router.post('/api/notes', (req, res) => {
+    console.log(req.body);
 
-//     if (!validateAnimal(req.body)) {
-//         res.status(400).send('The animal is not properly formatted.');
-//     } else {
-//         const animal = createNewAnimal(req.body, animals);
-//         res.json(animal);
-//     }
-// });
-
-
-
+    // create a modal, needs to reflect what is coming in (title&text) and also unique ID
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random() * 30)
+    }
+    notes.push(newNote);
+    fs.writeFileSync(
+        './db/notes.json',
+        JSON.stringify(notes, null, 2)
+    );
+    res.json(notes);
+    // return finished code to post route for respose
+    return body;
+});
 
 module.exports = router;
